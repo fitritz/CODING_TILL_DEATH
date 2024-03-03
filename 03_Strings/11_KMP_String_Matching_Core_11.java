785d87f525cdf71c099d77fd76f559d3118f38ac
fitritz <riteshbhogekar1234@gmail.com>
2024-03-03 12:00:00 +0530

    Add solution for KMP String Matching Core problem

---
+++ 03_Strings/11_KMP_String_Matching_Core_11.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class KMPStringMatchingCore11 {
+
+    static class Reader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                String line = br.readLine();
+                if (line == null) return null;
+                st = new StringTokenizer(line);
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    static int[] lps(String p){
+        int n=p.length(); int[] l=new int[n];
+        for(int i=1,j=0;i<n;i++){
+            while(j>0&&p.charAt(i)!=p.charAt(j)) j=l[j-1];
+            if(p.charAt(i)==p.charAt(j)) j++;
+            l[i]=j;
+        }
+        return l;
+    }
+    public static void main(String[] args) throws IOException {
+        Reader input = new Reader();
+        String t = input.next(), p = input.next();
+        int[] l = lps(p);
+        for(int i=0,j=0;i<t.length();i++){
+            while(j>0&&t.charAt(i)!=p.charAt(j)) j=l[j-1];
+            if(t.charAt(i)==p.charAt(j)) j++;
+            if(j==p.length()){ System.out.println(i-j+1); return; }
+        }
+        System.out.println(-1);
+    }
+
+}
