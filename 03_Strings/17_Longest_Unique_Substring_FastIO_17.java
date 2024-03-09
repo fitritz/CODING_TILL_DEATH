809104deeee1b96331fa480c867cd12ca713c093
fitritz <riteshbhogekar1234@gmail.com>
2024-03-09 12:00:00 +0530

    Optimize Longest Unique Substring FastIO in Strings

---
+++ 03_Strings/17_Longest_Unique_Substring_FastIO_17.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+class LongestUniqueSubstringFastIO17 {
+
+    static class InputFast {
+        private final InputStream in;
+        private final byte[] buf = new byte[1 << 15];
+        private int ptr = 0, len = 0;
+
+        InputFast(InputStream is) {
+            in = is;
+        }
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int val = 0;
+            while (c > 32) {
+                val = val * 10 + c - '0';
+                c = read();
+            }
+            return val * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            StringBuilder s = new StringBuilder();
+            while (c > 32) {
+                s.append((char) c);
+                c = read();
+            }
+            return s.toString();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        InputFast ip = new InputFast(System.in);
+        String s = ip.next();
+        int[] last = new int[256];
+        Arrays.fill(last, -1);
+        int l = 0, ans = 0;
+        for (int r = 0; r < s.length(); r++) {
+            int c = s.charAt(r);
+            if (last[c] >= l) l = last[c] + 1;
+            last[c] = r;
+            ans = Math.max(ans, r - l + 1);
+        }
+        System.out.println(ans);
+    }
+
+}
