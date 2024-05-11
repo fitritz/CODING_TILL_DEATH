5b7f19be0684660e427dc542756fd4c7145ec7ce
fitritz <riteshbhogekar1234@gmail.com>
2024-05-11 12:00:00 +0530

    DSA daily: implement Reverse Array Practice

---
+++ 06_Recursion/02_Reverse_Array_Practice_02.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class ReverseArrayPractice02 {
+
+    static class IO {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        IO sc = new IO();
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+        StringBuilder out = new StringBuilder();
+        for (int i = n - 1; i >= 0; i--) {
+            if (out.length() > 0) out.append(' ');
+            out.append(a[i]);
+        }
+        System.out.println(out);
+    }
+
+}
