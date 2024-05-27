ba8a089fb052e95d11789a9c019ee22e39b6a1e4
fitritz <riteshbhogekar1234@gmail.com>
2024-05-27 12:00:00 +0530

    DSA daily: refine Reverse Array Drill

---
+++ 06_Recursion/18_Reverse_Array_Drill_18.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class ReverseArrayDrill18 {
+
+    static class Fs {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Fs sc = new Fs();
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+        StringBuilder out = new StringBuilder();
+        for (int i = n - 1; i >= 0; i--) {
+            if (out.length() > 0) out.append(' ');
+            out.append(a[i]);
+        }
+        System.out.println(out);
+    }
+
+}
