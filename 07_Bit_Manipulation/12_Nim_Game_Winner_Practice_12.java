26e427a32ca19647f2117ecb02fdb497a5ded334
fitritz <riteshbhogekar1234@gmail.com>
2024-06-16 12:00:00 +0530

    DSA daily: refine Nim Game Winner Practice

---
+++ 07_Bit_Manipulation/12_Nim_Game_Winner_Practice_12.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class NimGameWinnerPractice12 {
+
+    static class IO {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        IO io = new IO();
+        int n = io.nextInt(), x = 0;
+        for (int i = 0; i < n; i++) x ^= io.nextInt();
+        System.out.println(x == 0 ? "Second" : "First");
+    }
+
+}
