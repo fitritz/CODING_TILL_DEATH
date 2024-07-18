bf538268f2d73d87fbcaf1e35e333bdcfce7e90c
fitritz <riteshbhogekar1234@gmail.com>
2024-07-18 12:00:00 +0530

    DSA daily: code Array Sum Drill

---
+++ 08_Linked_List/18_Array_Sum_Drill_18.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class ArraySumDrill18 {
+
+    static class FastIn {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastIn io = new FastIn();
+        int n = io.nextInt();
+        long ans = 0;
+        for (int i = 0; i < n; i++) ans += io.nextInt();
+        System.out.println(ans);
+    }
+
+}
