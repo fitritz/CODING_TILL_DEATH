0070e971b99d7f1d6b50a1bad64dc1a90acf5a21
fitritz <riteshbhogekar1234@gmail.com>
2024-07-26 12:00:00 +0530

    DSA daily: refine Array Sum Optimized

---
+++ 08_Linked_List/26_Array_Sum_Optimized_26.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class ArraySumOptimized26 {
+
+    static class Fs {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Fs io = new Fs();
+        int n = io.nextInt();
+        long ans = 0;
+        for (int i = 0; i < n; i++) ans += io.nextInt();
+        System.out.println(ans);
+    }
+
+}
