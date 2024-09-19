6e81d48eb6a7c2791dfa30eb2170a5e8c6982891
fitritz <riteshbhogekar1234@gmail.com>
2024-09-19 12:00:00 +0530

    Add solution for Binary Search Interview problem

---
+++ 11_Binary_Search/03_Binary_Search_Interview_03.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class BinarySearchInterview03 {
+
+    static class DataIn {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                String line = br.readLine();
+                if (line == null) return null;
+                st = new StringTokenizer(line);
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        DataIn input = new DataIn();
+        int n = input.nextInt(), t = input.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = input.nextInt();
+        int l = 0, r = n - 1, ans = -1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (a[m] == t) { ans = m; break; }
+            if (a[m] < t) l = m + 1; else r = m - 1;
+        }
+        System.out.println(ans);
+    }
+
+}
