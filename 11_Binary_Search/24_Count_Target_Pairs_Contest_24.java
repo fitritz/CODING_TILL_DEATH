5d57fcb904853f360951276e5d8cb9019491bb82
fitritz <riteshbhogekar1234@gmail.com>
2024-10-10 12:00:00 +0530

    DSA daily: complete Count Target Pairs Contest

---
+++ 11_Binary_Search/24_Count_Target_Pairs_Contest_24.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class CountTargetPairsContest24 {
+
+    static class FastIn {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastIn sc = new FastIn();
+        int n = sc.nextInt(), target = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+        Arrays.sort(a);
+        int l = 0, r = n - 1; long ans = 0;
+        while (l < r) {
+            long s = (long) a[l] + a[r];
+            if (s == target) { ans++; l++; r--; }
+            else if (s < target) l++; else r--;
+        }
+        System.out.println(ans);
+    }
+
+}
