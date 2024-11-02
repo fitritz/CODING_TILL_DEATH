b913e39256a64017959c3b88dc20753f2a1d8236
fitritz <riteshbhogekar1234@gmail.com>
2024-11-02 12:00:00 +0530

    Finish Two Sum Core problem

---
+++ 12_Hashing/21_Two_Sum_Core_21.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+class TwoSumCore21 {
+
+    static class QuickReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                String line = br.readLine();
+                if (line == null) return null;
+                st = new StringTokenizer(line);
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        QuickReader rd = new QuickReader();
+        int n = rd.nextInt(), target = rd.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = rd.nextInt();
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int need = target - a[i];
+            if (mp.containsKey(need)) { System.out.println(mp.get(need) + " " + i); return; }
+            mp.putIfAbsent(a[i], i);
+        }
+        System.out.println("-1 -1");
+    }
+
+}
