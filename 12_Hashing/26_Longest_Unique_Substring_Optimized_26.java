ce9c3cce58e7de1201e4ce349a4169aad42a6cef
fitritz <riteshbhogekar1234@gmail.com>
2024-11-07 12:00:00 +0530

    DSA daily: implement Longest Unique Substring Optimized

---
+++ 12_Hashing/26_Longest_Unique_Substring_Optimized_26.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class LongestUniqueSubstringOptimized26 {
+
+    static class IO {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        IO io = new IO();
+        String s = io.next();
+        int[] last = new int[256];
+        Arrays.fill(last, -1);
+        int l = 0, ans = 0;
+        for (int r = 0; r < s.length(); r++) {
+            int c = s.charAt(r);
+            if (last[c] >= l) l = last[c] + 1;
+            last[c] = r;
+            ans = Math.max(ans, r - l + 1);
+        }
+        System.out.println(ans);
+    }
+
+}
