b7306413d03c61f0d498a61bafdf3379fa486fcb
fitritz <riteshbhogekar1234@gmail.com>
2024-11-15 12:00:00 +0530

    DSA daily: code Reverse Array Drill

---
+++ 13_Two_Pointers/08_Reverse_Array_Drill_08.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class ReverseArrayDrill08 {
+
+    static class FastIn {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastIn io = new FastIn();
+        int n = io.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = io.nextInt();
+        StringBuilder out = new StringBuilder();
+        for (int i = n - 1; i >= 0; i--) {
+            if (out.length() > 0) out.append(' ');
+            out.append(a[i]);
+        }
+        System.out.println(out);
+    }
+
+}
