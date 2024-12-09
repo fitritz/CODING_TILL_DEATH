6a2803a8975ca99afe2d387a5b9717d1b8788d93
fitritz <riteshbhogekar1234@gmail.com>
2024-12-09 12:00:00 +0530

    DSA daily: complete Sliding Window Maximum Optimized

---
+++ 14_Sliding_Window/06_Sliding_Window_Maximum_Optimized_06.java
@@ -0,0 +1,34 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+class SlidingWindowMaximumOptimized06 {
+
+    static class FastIn {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastIn io = new FastIn();
+        int n = io.nextInt(), k = io.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = io.nextInt();
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) dq.pollFirst();
+            while (!dq.isEmpty() && a[dq.peekLast()] <= a[i]) dq.pollLast();
+            dq.offerLast(i);
+            if (i >= k - 1) { if (out.length() > 0) out.append(' '); out.append(a[dq.peekFirst()]); }
+        }
+        System.out.println(out);
+    }
+
+}
