7450985fb40130cf2a1f9f13b938e0f74e096a37
fitritz <riteshbhogekar1234@gmail.com>
2025-01-12 12:00:00 +0530

    Trees: refine Array Sum Contest

---
+++ 15_Trees/14_Array_Sum_Contest_14.java
@@ -0,0 +1,56 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+class ArraySumContest14 {
+
+    static class Scan {
+        private final BufferedInputStream in = new BufferedInputStream(System.in);
+        private final byte[] data = new byte[1 << 16];
+        private int idx = 0, size = 0;
+
+        private int read() throws IOException {
+            if (idx >= size) {
+                size = in.read(data);
+                idx = 0;
+                if (size < 0) return -1;
+            }
+            return data[idx++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int x = 0;
+            while (c > 32) {
+                x = x * 10 + (c - '0');
+                c = read();
+            }
+            return x * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            StringBuilder sb = new StringBuilder();
+            while (c > 32) {
+                sb.append((char) c);
+                c = read();
+            }
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scan in = new Scan();
+        int n = in.nextInt();
+        long ans = 0;
+        for (int i = 0; i < n; i++) ans += in.nextInt();
+        System.out.println(ans);
+    }
+
+}
