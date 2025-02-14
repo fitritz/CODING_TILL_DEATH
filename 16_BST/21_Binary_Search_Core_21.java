f0c9b3646e7ff0d7f9da53dff8adf963a111b57b
fitritz <riteshbhogekar1234@gmail.com>
2025-02-14 12:00:00 +0530

    Solve Binary Search Core problem

---
+++ 16_BST/21_Binary_Search_Core_21.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class BinarySearchCore21 {
+
+    static class Reader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                String line = br.readLine();
+                if (line == null) return null;
+                st = new StringTokenizer(line);
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Reader rd = new Reader();
+        int n = rd.nextInt(), t = rd.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = rd.nextInt();
+        int l = 0, r = n - 1, ans = -1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (a[m] == t) { ans = m; break; }
+            if (a[m] < t) l = m + 1; else r = m - 1;
+        }
+        System.out.println(ans);
+    }
+
+}
