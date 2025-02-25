f2616a0794686605a3996cbf9dfde3672fd6309b
fitritz <riteshbhogekar1234@gmail.com>
2025-02-25 12:00:00 +0530

    Heap: implement Kth Largest Element Optimized

---
+++ 17_Heap/06_Kth_Largest_Element_Optimized_06.java
@@ -0,0 +1,61 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.util.PriorityQueue;
+
+class KthLargestElementOptimized06 {
+
+    static class Input {
+        private final BufferedInputStream in = new BufferedInputStream(System.in);
+        private final byte[] data = new byte[1 << 16];
+        private int idx = 0, size = 0;
+
+        private int read() throws IOException {
+            if (idx >= size) {
+                size = in.read(data);
+                idx = 0;
+                if (size < 0) return -1;
+            }
+            return data[idx++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int x = 0;
+            while (c > 32) {
+                x = x * 10 + (c - '0');
+                c = read();
+            }
+            return x * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            StringBuilder sb = new StringBuilder();
+            while (c > 32) {
+                sb.append((char) c);
+                c = read();
+            }
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Input reader = new Input();
+        int n = reader.nextInt(), k = reader.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int x = reader.nextInt();
+            if (pq.size() < k) pq.offer(x);
+            else if (x > pq.peek()) { pq.poll(); pq.offer(x); }
+        }
+        System.out.println(pq.peek());
+    }
+
+}
