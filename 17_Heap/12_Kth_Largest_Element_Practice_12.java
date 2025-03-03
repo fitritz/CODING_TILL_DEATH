fc4668065c338a4c068363287634659b4784b8ef
fitritz <riteshbhogekar1234@gmail.com>
2025-03-03 12:00:00 +0530

    DSA daily: refine Kth Largest Element Practice

---
+++ 17_Heap/12_Kth_Largest_Element_Practice_12.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class KthLargestElementPractice12 {
+
+    static class FastIn {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastIn sc = new FastIn();
+        int n = sc.nextInt(), k = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            if (pq.size() < k) pq.offer(x);
+            else if (x > pq.peek()) { pq.poll(); pq.offer(x); }
+        }
+        System.out.println(pq.peek());
+    }
+
+}
