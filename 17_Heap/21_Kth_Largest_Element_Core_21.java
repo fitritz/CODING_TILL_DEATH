48ccd5d814b40d0bfcf4b667f7f7da1a20d72b30
fitritz <riteshbhogekar1234@gmail.com>
2025-03-12 12:00:00 +0530

    Finish Kth Largest Element Core in Heap

---
+++ 17_Heap/21_Kth_Largest_Element_Core_21.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.PriorityQueue;
+
+class KthLargestElementCore21 {
+
+    static class In {
+        private final InputStream in;
+        private final byte[] buf = new byte[1 << 15];
+        private int ptr = 0, len = 0;
+
+        In(InputStream is) {
+            in = is;
+        }
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int val = 0;
+            while (c > 32) {
+                val = val * 10 + c - '0';
+                c = read();
+            }
+            return val * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            StringBuilder s = new StringBuilder();
+            while (c > 32) {
+                s.append((char) c);
+                c = read();
+            }
+            return s.toString();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        In ip = new In(System.in);
+        int n = ip.nextInt(), k = ip.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int x = ip.nextInt();
+            if (pq.size() < k) pq.offer(x);
+            else if (x > pq.peek()) { pq.poll(); pq.offer(x); }
+        }
+        System.out.println(pq.peek());
+    }
+
+}
