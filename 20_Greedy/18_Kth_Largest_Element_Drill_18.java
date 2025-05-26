bd5fa679d9743bff0b5bc119cc2eb2361746f7f3
fitritz <riteshbhogekar1234@gmail.com>
2025-05-26 12:00:00 +0530

    DSA daily: implement Kth Largest Element Drill

---
+++ 20_Greedy/18_Kth_Largest_Element_Drill_18.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class KthLargestElementDrill18 {
+
+    static class FastIn {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastIn io = new FastIn();
+        int n = io.nextInt(), k = io.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int x = io.nextInt();
+            if (pq.size() < k) pq.offer(x);
+            else if (x > pq.peek()) { pq.poll(); pq.offer(x); }
+        }
+        System.out.println(pq.peek());
+    }
+
+}
