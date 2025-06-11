920cf0f899061a8c9d2ee2769821e0699ca62e39
fitritz <riteshbhogekar1234@gmail.com>
2025-06-11 12:00:00 +0530

    DSA daily: refine Kth Largest Element Drill

---
+++ 21_Dynamic_Programming/08_Kth_Largest_Element_Drill_08.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class KthLargestElementDrill08 {
+
+    static class IO {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        IO io = new IO();
+        int n = io.nextInt(), k = io.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int x = io.nextInt();
+            if (pq.size() < k) pq.offer(x);
+            else if (x > pq.peek()) { pq.poll(); pq.offer(x); }
+        }
+        System.out.println(pq.peek());
+    }
+
+}
