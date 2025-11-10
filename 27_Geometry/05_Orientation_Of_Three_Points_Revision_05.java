7b1b2b52bd0202d8d71e34762b153b0b8d9101c0
fitritz <riteshbhogekar1234@gmail.com>
2025-11-10 12:00:00 +0530

    DSA daily: code Orientation Of Three Points Revision

---
+++ 27_Geometry/05_Orientation_Of_Three_Points_Revision_05.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class OrientationOfThreePointsRevision05 {
+
+    static class FastIn {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    static long cross(long x1,long y1,long x2,long y2,long x3,long y3){
+        return (x2-x1)*(y3-y1)-(y2-y1)*(x3-x1);
+    }
+    public static void main(String[] args) throws Exception {
+        FastIn io = new FastIn();
+        long x1=io.nextInt(),y1=io.nextInt(),x2=io.nextInt(),y2=io.nextInt(),x3=io.nextInt(),y3=io.nextInt();
+        long c = cross(x1,y1,x2,y2,x3,y3);
+        if (c==0) System.out.println("COLLINEAR");
+        else if (c>0) System.out.println("COUNTER_CLOCKWISE");
+        else System.out.println("CLOCKWISE");
+    }
+
+}
