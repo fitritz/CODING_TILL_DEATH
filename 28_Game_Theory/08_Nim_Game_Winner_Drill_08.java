7d66830f8e1f482e1efe7533a6b3af988444d3f4
fitritz <riteshbhogekar1234@gmail.com>
2025-12-08 12:00:00 +0530

    DSA daily: refine Nim Game Winner Drill

---
+++ 28_Game_Theory/08_Nim_Game_Winner_Drill_08.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class NimGameWinnerDrill08 {
+
+    static class IO {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        IO sc = new IO();
+        int n = sc.nextInt(), x = 0;
+        for (int i = 0; i < n; i++) x ^= sc.nextInt();
+        System.out.println(x == 0 ? "Second" : "First");
+    }
+
+}
