4235b1359dcec32dd96d73eeb2891b4fe0599f30
fitritz <riteshbhogekar1234@gmail.com>
2025-12-19 12:00:00 +0530

    Optimize Array Sum Mastery problem

---
+++ 28_Game_Theory/19_Array_Sum_Mastery_19.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class ArraySumMastery19 {
+
+    static class DataIn {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                String line = br.readLine();
+                if (line == null) return null;
+                st = new StringTokenizer(line);
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        DataIn input = new DataIn();
+        int n = input.nextInt();
+        long ans = 0;
+        for (int i = 0; i < n; i++) ans += input.nextInt();
+        System.out.println(ans);
+    }
+
+}
