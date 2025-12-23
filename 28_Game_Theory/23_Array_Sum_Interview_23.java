d6c4b8e0d1297a71dfd042dc2f2ce1452c6da22d
fitritz <riteshbhogekar1234@gmail.com>
2025-12-23 12:00:00 +0530

    Add solution for Array Sum Interview problem

---
+++ 28_Game_Theory/23_Array_Sum_Interview_23.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class ArraySumInterview23 {
+
+    static class Reader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                String line = br.readLine();
+                if (line == null) return null;
+                st = new StringTokenizer(line);
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Reader rd = new Reader();
+        int n = rd.nextInt();
+        long ans = 0;
+        for (int i = 0; i < n; i++) ans += rd.nextInt();
+        System.out.println(ans);
+    }
+
+}
