8c6b5dea24e314e41d93b7b51231aaeff843508c
fitritz <riteshbhogekar1234@gmail.com>
2026-01-03 12:00:00 +0530

    String Algorithms: complete KMP String Matching Mastery

---
+++ 29_String_Algorithms/09_KMP_String_Matching_Mastery_09.java
@@ -0,0 +1,69 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+class KMPStringMatchingMastery09 {
+
+    static class Input {
+        private final BufferedInputStream in = new BufferedInputStream(System.in);
+        private final byte[] data = new byte[1 << 16];
+        private int idx = 0, size = 0;
+
+        private int read() throws IOException {
+            if (idx >= size) {
+                size = in.read(data);
+                idx = 0;
+                if (size < 0) return -1;
+            }
+            return data[idx++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int x = 0;
+            while (c > 32) {
+                x = x * 10 + (c - '0');
+                c = read();
+            }
+            return x * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            do c = read(); while (c <= 32 && c != -1);
+            StringBuilder sb = new StringBuilder();
+            while (c > 32) {
+                sb.append((char) c);
+                c = read();
+            }
+            return sb.toString();
+        }
+    }
+
+    static int[] lps(String p){
+        int n=p.length(); int[] l=new int[n];
+        for(int i=1,j=0;i<n;i++){
+            while(j>0&&p.charAt(i)!=p.charAt(j)) j=l[j-1];
+            if(p.charAt(i)==p.charAt(j)) j++;
+            l[i]=j;
+        }
+        return l;
+    }
+    public static void main(String[] args) throws Exception {
+        Input reader = new Input();
+        String t = reader.next(), p = reader.next();
+        int[] l = lps(p);
+        for(int i=0,j=0;i<t.length();i++){
+            while(j>0&&t.charAt(i)!=p.charAt(j)) j=l[j-1];
+            if(t.charAt(i)==p.charAt(j)) j++;
+            if(j==p.length()){ System.out.println(i-j+1); return; }
+        }
+        System.out.println(-1);
+    }
+
+}
