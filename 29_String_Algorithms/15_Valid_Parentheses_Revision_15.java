51e756e08ba7a7bf67d43e5b2b22185e056fc359
fitritz <riteshbhogekar1234@gmail.com>
2026-01-09 12:00:00 +0530

    DSA daily: code Valid Parentheses Revision

---
+++ 29_String_Algorithms/15_Valid_Parentheses_Revision_15.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+class ValidParenthesesRevision15 {
+
+    static class FastIn {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    static boolean ok(char a, char b) {
+        return (a=='('&&b==')')||(a=='['&&b==']')||(a=='{'&&b=='}');
+    }
+    public static void main(String[] args) throws Exception {
+        FastIn sc = new FastIn();
+        String s = sc.next();
+        ArrayDeque<Character> st = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c=='('||c=='['||c=='{') st.push(c);
+            else if (st.isEmpty() || !ok(st.pop(), c)) { System.out.println("false"); return; }
+        }
+        System.out.println(st.isEmpty() ? "true" : "false");
+    }
+
+}
