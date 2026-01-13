a002ee6a8518f92bcdef1d7abc66c71488a38fa6
fitritz <riteshbhogekar1234@gmail.com>
2026-01-13 12:00:00 +0530

    DSA daily: complete Valid Parentheses Mastery

---
+++ 29_String_Algorithms/19_Valid_Parentheses_Mastery_19.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+class ValidParenthesesMastery19 {
+
+    static class IO {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    static boolean ok(char a, char b) {
+        return (a=='('&&b==')')||(a=='['&&b==']')||(a=='{'&&b=='}');
+    }
+    public static void main(String[] args) throws IOException {
+        IO io = new IO();
+        String s = io.next();
+        ArrayDeque<Character> st = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c=='('||c=='['||c=='{') st.push(c);
+            else if (st.isEmpty() || !ok(st.pop(), c)) { System.out.println("false"); return; }
+        }
+        System.out.println(st.isEmpty() ? "true" : "false");
+    }
+
+}
