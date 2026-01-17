f8f3621a11771f05cdf0a01b4862ca56fe2530ae
fitritz <riteshbhogekar1234@gmail.com>
2026-01-17 12:00:00 +0530

    DSA daily: refine Valid Parentheses Interview

---
+++ 29_String_Algorithms/23_Valid_Parentheses_Interview_23.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+class ValidParenthesesInterview23 {
+
+    static class Fs {
+        private final Scanner sc = new Scanner(System.in);
+
+        int nextInt() {
+            return sc.nextInt();
+        }
+
+        String next() {
+            return sc.next();
+        }
+    }
+
+    static boolean ok(char a, char b) {
+        return (a=='('&&b==')')||(a=='['&&b==']')||(a=='{'&&b=='}');
+    }
+    public static void main(String[] args) throws IOException {
+        Fs sc = new Fs();
+        String s = sc.next();
+        ArrayDeque<Character> st = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c=='('||c=='['||c=='{') st.push(c);
+            else if (st.isEmpty() || !ok(st.pop(), c)) { System.out.println("false"); return; }
+        }
+        System.out.println(st.isEmpty() ? "true" : "false");
+    }
+
+}
